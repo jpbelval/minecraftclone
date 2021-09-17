88c47c96e303fcafcf726fd5615b7747e148c45a
jaypey <jp@belval.org>
2021-09-17 09:51:05 -0400

    Ajout de l'initialisation en AIR dans le constructeur

--- blockarray3d.cpp
+++ blockarray3d.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include "blockarray3d.h"
 
+BlockArray3d::BlockArray3d(int x, int y, int z){
+    m_x = x;
+    m_y = y;
+    m_z = z;
+    m_blocks = new BlockType[m_x * m_y * m_z];
+    Reset(BTYPE_AIR);
+}
+
+BlockArray3d::~BlockArray3d(){
+    delete[] m_blocks;
+}
+
+BlockArray3d::BlockArray3d(const BlockArray3d& blockArrayOrigin){
+    m_x = blockArrayOrigin.m_x;
+    m_y = blockArrayOrigin.m_y;
+    m_z = blockArrayOrigin.m_z;
+    m_blocks = new BlockType[m_x * m_y * m_z];
+}
 
 BlockType BlockArray3d::Get(int x, int y, int z) const{
-    return m_blocks [ x + ( z * m_x ) + ( y * m_z * m_x ) ];
+    return m_blocks[ x + ( z * m_x ) + ( y * m_z * m_x ) ];
 }
 
 void BlockArray3d::Set(int x, int y, int z, BlockType type){
@@ -12,17 +30,9 @@ void BlockArray3d::Set(int x, int y, int z, BlockType type){
 
 void BlockArray3d::Reset(BlockType type){
     for (int i = 0; i < m_x * m_y * m_z; i++)
+    {
         m_blocks[i] = type;
+    }
     
 }
 
-BlockArray3d::BlockArray3d(int x, int y, int z){
-    m_x = x;
-    m_y = y;
-    m_z = z;
-    m_blocks = new BlockType[m_x * m_y * m_z];
-}
-
-BlockArray3d::~BlockArray3d(){
-    delete[] m_blocks;
-}
