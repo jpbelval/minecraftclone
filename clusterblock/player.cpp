6854ed6fd2a86611206a2ef837c2cd3a885d102e
jaypey <jp@belval.org>
2021-10-01 08:25:36 -0400

    Ajout de la class player

---
+++ clusterblock/player.cpp
@@ -0,0 +1,55 @@
+
+#include "player.h"
+
+Player::Player(const Vector3f& position , float rotX = 0, float rotY = 0) : m_Position(position), m_RotX(rotX), m_RotY(rotY)
+{
+
+}
+
+
+Player::~Player(){
+
+}
+
+void Player::TurnLeftRight(float value){
+
+}
+
+void Player::TurnTopBottom(float value){
+
+}
+
+void Player::Move(bool front , bool back , bool left , bool right , float elapsedTime){
+    if(front){
+        float xrotrad, yrotrad;
+        yrotrad = (m_RotY / 180 * 3.141592654f);
+        xrotrad = (m_RotX / 180 * 3.141592654f);
+        m_Position.x += float(sin(yrotrad));
+        m_Position.y += float(sin(xrotrad));
+        m_Position.z += float(sin(yrotrad));
+    }
+    else if(back){
+        float xrotrad, yrotrad;
+        yrotrad = (m_RotY / 180 * 3.141592654f);
+        xrotrad = (m_RotX / 180 * 3.141592654f);
+        m_Position.x -= float(sin(yrotrad));
+        m_Position.z += float(cos(yrotrad)) ;
+        m_Position.y += float(sin(xrotrad));
+    }
+    else if(right){
+        float yrotrad;
+        yrotrad = (m_RotY / 180 * 3.141592654f);
+        m_Position.x += float(cos(yrotrad)) * 0.2;
+        m_Position.z += float(sin(yrotrad)) * 0.2;
+    }
+    else if(right){
+        float yrotrad;
+        yrotrad = (m_RotY / 180 * 3.141592654f);
+        m_Position.x -= float(cos(yrotrad)) * 0.2;
+        m_Position.z -= float(sin(yrotrad)) * 0.2;
+    }
+}
+
+void Player::ApplyTransformation(Transformation& transformation) const{
+
+}
---
+++ clusterblock/player.h
@@ -0,0 +1,24 @@
+#ifndef PLAYER_H__
+#define PLAYER_H__
+
+#include "vector3.h"
+#include "transformation.h"
+
+class Player
+{
+private:
+    Vector3f m_Position;
+    float m_RotX;
+    float m_RotY;
+public:
+    Player(const Vector3f& position , float rotX = 0, float rotY = 0);
+    ~Player();
+
+    void TurnLeftRight(float value);
+    void TurnTopBottom(float value);
+    void Move(bool front , bool back , bool left , bool right , float elapsedTime);
+    void ApplyTransformation(Transformation& transformation) const;
+
+};
+
+#endif
