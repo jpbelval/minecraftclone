9924c2105f2b1aafd841bd6509e5c957e9996625
jaypey <jp@belval.org>
2021-11-01 11:34:03 -0400

    Changement des array

--- clusterblock/array2d.h
+++ clusterblock/array2d.h
@@ -21,7 +21,44 @@ class Array2d
 
     private:
         int m_x, m_y;
-        T* m_blocks;
+        T* m_data;
 };
+
+template<class T>
+Array2d<T>::Array2d(int x, int y) : m_x(x), m_y(y)
+{
+    m_data = new T[m_x * m_y];
+}
+
+template<class T>
+Array2d<T>::~Array2d()
+{
+    delete [] m_data;
+}
+
+template<class T>
+Array2d<T>::Array2d(const Array2d& array) : m_x(array.m_x), m_y(array.m_y)
+{
+    m_data = new T[m_x * m_y];
+    for(int i = 0; i < m_x * m_y; ++i)
+        m_data[i] = array.m_data[i];
+}
+template<class T>
+void Array2d<T>::Set(int x, int y, T type)
+{
+    m_data[To1dIndex(x, y)] = type;
+}
+
+template<class T>
+T Array2d<T>::Get(int x, int y) const
+{
+    return m_data[To1dIndex(x, y)];
+}
+
+template<class T>
+int Array2d<T>::To1dIndex(int x, int y) const
+{
+    return y + (x * m_x);
+}
 
 #endif
--- clusterblock/array3d.h
+++ clusterblock/array3d.h
@@ -21,47 +21,38 @@ class Array3d
 
     private:
         int m_x, m_y, m_z;
-        T* m_blocks;
+        T* m_data;
 };
 
 template<class T>
 Array3d<T>::Array3d(int x, int y, int z) : m_x(x), m_y(y), m_z(z)
 {
-    m_blocks = new BlockType[m_x * m_y * m_z];
-
-    Reset(BTYPE_AIR);
+    m_data = new T[m_x * m_y * m_z];
 }
 
 template<class T>
 Array3d<T>::~Array3d()
 {
-    delete [] m_blocks;
+    delete [] m_data;
 }
 
 template<class T>
 Array3d<T>::Array3d(const Array3d& array) : m_x(array.m_x), m_y(array.m_y), m_z(array.m_z)
 {
-    m_blocks = new T[m_x * m_y * m_z];
+    m_data = new T[m_x * m_y * m_z];
     for(int i = 0; i < m_x * m_y * m_z; ++i)
-        m_blocks[i] = array.m_blocks[i];
+        m_data[i] = array.m_data[i];
 }
 template<class T>
 void Array3d<T>::Set(int x, int y, int z, T type)
 {
-    m_blocks[To1dIndex(x, y, z)] = type;
+    m_data[To1dIndex(x, y, z)] = type;
 }
 
 template<class T>
 T Array3d<T>::Get(int x, int y, int z) const
 {
-    return m_blocks[To1dIndex(x, y, z)];
-}
-
-template<class T>
-void Array3d<T>::Reset(T type)
-{
-    for(int i = 0; i < m_x * m_y * m_z; ++i)
-        m_blocks[i] = type;
+    return m_data[To1dIndex(x, y, z)];
 }
 
 template<class T>
